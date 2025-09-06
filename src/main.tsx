be43d179fd3cdb85175c2d97019d0d80f64c1f79
aein <mananniaein1234@gmail.com>
2025-09-06 18:31:46 +0330

    add new Chnage

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import './i18n.ts';
+import backgroundImage from '../public/images/background2.webp';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <main className="h-screen w-full overflow-hidden border-2 border-blue-500">
+    <main
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+      className="h-screen w-full overflow-hidden border-2 border-blue-500 bg-center bg-no-repeat bg-cover"
+    >
       <App />
     </main>
   </StrictMode>
--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -3,7 +3,7 @@ import { useChangeLanguage } from '../../hooks/useChangeLanguage';
 export default function LandingPage() {
   const { t, i18n } = useChangeLanguage();
   return (
-    <div className="p-4">
+    <div className="p-4 bg-green-200/50 backdrop-filter backdrop-blur-md ">
       <h1 className="text-2xl font-bold">{t('welcome')}</h1>
       <div className="mt-4 flex gap-2">
         <button onClick={() => i18n.changeLanguage('en')}>EN</button>
