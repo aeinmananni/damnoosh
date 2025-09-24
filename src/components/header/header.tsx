d4283e956b6219e7127d0b72fe514950a5e19daa
aein <mananniaein1234@gmail.com>
2025-09-25 02:31:46 +0330

    ww

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,7 @@ import { SANavbar, SAWidget } from './components';
 import logo from '../../images/result.png';
 export default function Header() {
   return (
-    <header className="w-full flex gap-3 p-3">
+    <header className="w-full flex gap-3 ">
       <SAImage src={logo} alt="Image-Logo" width={60} height={60} className="!shrink-0 border" />
       <div className="flex flex-col gap-9 w-full px-4">
         <SAWidget />
--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -4,7 +4,7 @@ import { useChangeLanguage } from '../../hooks/useChangeLanguage';
 export default function LandingPage() {
   const { i18n } = useChangeLanguage();
   return (
-    <div className="flex flex-col w-full h-full ">
+    <div className="flex flex-col w-full h-full p-7">
       <SAHeader />
       <button onClick={() => i18n.changeLanguage('ar')}>ar</button>
       <button onClick={() => i18n.changeLanguage('de')}>de</button>
